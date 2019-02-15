d8dcba6e669fb4ae410b2d01bd19aadbbde7087b
XelaNull <xelanull@orbitalgrooves.com>
2019-02-14 21:36:21 -0500

    Initial

---
+++ loop_start_autoreveal.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+export INSTALL_DIR=/data/7DTD
+# If this script is already running, then exit
+if pidof -o %PPID -x "loop_start_autoreveal.sh"; then exit; fi
+# Loop until the 7dtd server initialization completes
+while true; do 
+  if [ -f /7dtd.initialized ]; then break; fi; sleep 6; 
+done
+# Loop forever
+while true; do 
+  if [[ -f $INSTALL_DIR/7DaysToDieServer.x86_64 ]] && [[ `cat $INSTALL_DIR/auto-reveal.status` == "start" ]]; then
+    SERVER_PID=`ps awwux | grep -v grep | grep 7DaysToDieServer.x86_64`;
+    [[ -z $SERVER_PID ]] && /data/7DTD/7dtd-auto-reveal-map/7dtd-run-after-initial-start.sh /data/7DTD
+  fi
+  sleep 2
+done
