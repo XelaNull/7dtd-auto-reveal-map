a00950003d16f83373fe1157c05d35ad85da2b0c
XelaNull <xelanull@orbitalgrooves.com>
2020-01-13 10:19:42 -0500

    bugfix to ensure no duplicate starts

--- loop_start_autoreveal.sh
+++ loop_start_autoreveal.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 export INSTALL_DIR=/data/7DTD
 # If this script is already running, then exit
-if pidof -o %PPID -x "loop_start_autoreveal.sh"; then exit; fi
+PIDTEST=`/usr/sbin/pidof -o %PPID -x "loop_start_autoreveal.sh"`
+if [[ $PIDTEST != "" ]]; then exit; fi
 # Loop until the 7dtd server initialization completes
-while true; do 
-  if [ -f /7dtd.initialized ]; then break; fi; sleep 6; 
+while true; do
+  if [ -f /7dtd.initialized ]; then break; fi; sleep 6;
 done
 # Loop forever
-while true; do 
+while true; do
   if [[ -f $INSTALL_DIR/7DaysToDieServer.x86_64 ]] && [[ `cat $INSTALL_DIR/auto-reveal.status` == "start" ]]; then
     SERVER_PID=`ps awwux | grep -v grep | grep 7DaysToDieServer.x86_64`;
     [[ ! -z $SERVER_PID ]] && /data/7DTD/7dtd-auto-reveal-map/7dtd-run-after-initial-start.sh /data/7DTD
